7544bd6130faa09e3e378418b9661342f1b47818
MayaShivaji <maaya.shivaji@gmail.com>
2023-01-17 20:03:55 -0800

    fixed commit (also change language)

--- audio_server.py
+++ audio_server.py
@@ -4,9 +4,8 @@ from translate import translate
 import speech_recognition as sr
 
 
-global_source_language = 'ja'
-global_target_language = 'en'
-name = 'Tori'
+
+name = 'tori'
 
 # when the name hey utrans is called, it stops translating and instead follows a number of commands
     #change_tlang [completed] (target language) **can use string to find specific word so it triggers without specific command
@@ -45,23 +44,27 @@ def intro():
 language_mapping = {'Spanish': 'es', 'Russian':	'ru','Portuguese': 'pt','English': 'en','Japanese': 'ja', 'Chinese': 'zh'}
 
 
-def call_t(said):
+def call_t(said, global_source_language, global_target_language):
 #changes target language by calling name
     if said and "target language" in said.lower():
         words = said.split(" ")
         target_language = words[-1]
         print("target_language: {}".format(language_mapping[target_language]))
         global_target_language = language_mapping[target_language]
-
+        tts("Changed target language to {}".format(target_language), "en" )
 
     if said and "my language" in said.lower():
         words = said.split(" ")
         my_language = words[-1]
         print("source_language: {}".format(language_mapping[my_language]))
         global_source_language = language_mapping[my_language]
+        tts("Changed source language to {}".format(my_language), "en")
+    return global_source_language, global_target_language
 
 
-def audio_processor():
+def audio_processor(global_source_language, global_target_language):
+    # global_source_language = 'en'
+    # global_target_language = 'es'
     r = sr.Recognizer()
     # tts("Hello donkeys", "es")
     print("Recording.. ")
@@ -70,29 +73,35 @@ def audio_processor():
         audio = r.listen(source)
         text = ""
         try:
+            print("global_source_language: ", global_source_language)
             said = r.recognize_google(audio, language=global_source_language)
         except:
             said = None
 
-        if said and "Tori" in said.lower():
+        if said and "language" in said.lower():
             # Process commands:
             #
             # 1. Set language to <language> ->
             # 2. Set target language to <new language>
             # tts("Oh, you called me?", "en")
-            call_t(said)
+            # call_t(said)
+            global_source_language, global_target_language = call_t(said, global_source_language, global_target_language)
 
-        # except:
-        #     pass
-        print("Said: {}".format(said))
-        try:
-            text = translate(said, global_target_language, src=global_source_language)
-            tts(text, global_target_language)
-        except:
-            pass
+        else:
+            # except:
+            #     pass
+            print("Said: {}".format(said))
+            try:
+                text = translate(said, global_target_language, src=global_source_language)
+                tts(text, global_target_language)
+            except:
+                pass
+        return global_source_language, global_target_language
 
 if __name__ == "__main__":
     # intro()
+    global_source_language = "en"
+    global_target_language = "es"
     while True:
-        audio_processor()
+        global_source_language, global_target_language = audio_processor(global_source_language=global_source_language, global_target_language=global_target_language)
         # print(text)
